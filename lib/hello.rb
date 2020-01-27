1711344d169b8f10d250207411db4b7395df3a32
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 20:20:15 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def hello_t(["Tim","Tom","Jim"]) do |name|
+  if name.start_with?("T")
+     i = 0 
+  while i < array.lenght
+     yield array[i]
+    i = i + 1 
+  end
+  array 
 end
 
-# call your method here!
+
 
